3ba34a5d51dfe2d1292dfd4ddcd9d72f6492cd11
honghaozhilian <274486566@qq.com>
2018-07-06 09:29:24 +0800

    增加webpack-parallel-uglify-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 const commonConfig = require('./webpack.common.config');
 const prodConfig = {
     mode: 'production',
@@ -88,6 +89,21 @@ const prodConfig = {
         new MiniCssExtractPlugin({
             filename: 'static/css/[name].[hash].css',
             chunkFilename: 'static/css/[name][id].[hash].css',
+        }),
+        new ParallelUglifyPlugin({
+            workerCount: 4,
+            uglifyJS: {
+                output: {
+                    beautify: false, // 不需要格式化
+                    comments: false // 保留注释
+                },
+                compress: { // 压缩
+                    warnings: false, // 删除无用代码时不输出警告
+                    drop_console: true, // 删除console语句
+                    collapse_vars: true, // 内嵌定义了但是只有用到一次的变量
+                    reduce_vars: true // 提取出出现多次但是没有定义成变量去引用的静态值
+                }
+            }
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: 'static'
